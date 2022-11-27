e82cbd7b50d76a52365ceae7ebbfe1937c046519
imvipindev <vipinchaudhary1809@gmail.com>
2022-11-27 17:19:49 +0530

    feat(index.js): added json response

--- index.js
+++ index.js
@@ -2,7 +2,9 @@ const express = require("express");
 const app = express();
 
 app.get("/", (req, res) => {
-  console.log("It works");
+  res.json({
+    Id: "this work",
+  });
 });
 
 app.listen(3000, () => {
