d71746071dafb2418116d53ca1eddf1474ffe47f
imvipindev <vipinchaudhary1809@gmail.com>
2022-11-28 11:53:27 +0530

    chore(db.js): improve logs

--- config/db.js
+++ config/db.js
@@ -2,5 +2,5 @@ const mongoose = require("mongoose");
 require("dotenv").config();
 const { MONGO_URI } = process.env;
 module.exports = mongoose.connect(MONGO_URI, () => {
-  console.log("Connected to MongoDB");
+  console.log("Connected to Mongo DataBase");
 });
