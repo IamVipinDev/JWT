fe69246065188a138ec92b89d7778fe8bb031d17
imvipindev <vipinchaudhary1809@gmail.com>
2022-11-28 11:04:25 +0530

    fix(config): fixed bug

--- config/db.js
+++ config/db.js
@@ -1,14 +1,6 @@
 const mongoose = require("mongoose");
 require("dotenv").config();
 const { MONGO_URI } = process.env;
-module.exports = mongoose.connect(
-  MONGO_URI,
-  {
-    useNewUrlParser: true,
-    useFindAndModify: false,
-    useUnifiedTopology: true,
-  },
-  () => {
-    console.log("Connected to Database");
-  }
-);
+module.exports = mongoose.connect(MONGO_URI, () => {
+  console.log("Connected to MongoDB");
+});
