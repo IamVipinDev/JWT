1709a70b0cd7f4e6e48a230b284f0f92dd56353f
imvipindev <vipinchaudhary1809@gmail.com>
2022-11-28 11:54:44 +0530

    fix(index.js):remove basic handler

--- index.js
+++ index.js
@@ -5,12 +5,6 @@ const server = http.createServer(app);
 const { API_PORT } = process.env;
 const port = process.env.PORT || API_PORT;
 
-app.get("/", (req, res) => {
-  res.json({
-    greet: "hello vipin this work",
-  });
-});
-
 server.listen(port, () => {
   console.log(`Server started on http://localhost:${port}....`);
 });
